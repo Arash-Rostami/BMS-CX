bc6e19fc308de8779ff163ab3b3a76d120071830
Arash_Rostami_ <arashrostami@time-gr.com>
2024-03-12 17:59:31 +0330

    final payment creation, leading to status update

--- app/Filament/Resources/Operational/PaymentResource/Pages/CreatePayment.php
+++ app/Filament/Resources/Operational/PaymentResource/Pages/CreatePayment.php
@@ -7,62 +7,51 @@ use App\Models\PaymentRequest;
 use App\Models\User;
 use App\Services\NotificationManager;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+
 
 class CreatePayment extends CreateRecord
 {
     protected static string $resource = PaymentResource::class;
 
-    protected function mutateFormDataBeforeCreate(array $data): array
+
+    protected function handleRecordCreation(array $data): Model
     {
-        $paymentRequestIds = $data['payment_request_id'];
+        // Load PaymentRequest models
+        $paymentRequests = PaymentRequest::whereIn('id', $data['payment_request_id'])->get();
+
         $paymentAmount = $data['amount'];
         $totalAmountPaid = 0;
+        $lastModelInstance = null;
 
-        foreach ($paymentRequestIds as $index => $paymentRequestId) {
-
-            // If this is the last item, no need to continue the loop
-            if ($index === count($paymentRequestIds) - 1) {
-                break;
-            }
+        foreach ($paymentRequests as $paymentRequest) {
 
             // Calculate the remaining payable amount
             $payableAmount = $paymentAmount - $totalAmountPaid;
-
-            // Find the payment request to update
-            $paymentRequest = PaymentRequest::find($paymentRequestId);
 
             // Determine the amount to be paid for the current request
             $amountToPay = min($paymentRequest->individual_amount, $payableAmount);
 
             // Update the status if the full amount is covered
-            if ($amountToPay >= $paymentRequest->individual_amount) {
-                $paymentRequest->update(['status' => 'completed']);
-            }
+            $paymentRequest->update([
+                'status' => $amountToPay >= $paymentRequest->individual_amount ? 'completed' : 'processing',
+            ]);
 
             // Prepare the data for the new record
             $newData = [
-                    'payment_request_id' => $paymentRequestId,
+                    'payment_request_id' => $paymentRequest->id,
                     'amount' => $amountToPay,
                 ] + $data;
 
             // Create the new record
-            static::getModel()::create($newData);
+            $lastModelInstance = static::getModel()::create($newData);
 
             // Update the total amount paid
             $totalAmountPaid += $amountToPay;
         }
-
-        // Update the amount for the last payment request
-        $data['amount'] = $paymentAmount - $totalAmountPaid;
-        $data['payment_request_id'] = end($paymentRequestIds);
-
-        $finalPaymentRequest = PaymentRequest::find($data['payment_request_id']);
-        if ($data['amount'] >= $finalPaymentRequest->individual_amount) {
-            $finalPaymentRequest->update(['status' => 'completed']);
-        }
-
-        return $data;
+        return $lastModelInstance;
     }
+
 
     protected function afterCreate(): void
     {
